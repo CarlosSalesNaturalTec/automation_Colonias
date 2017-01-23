cf1cac69b435655295b5143de382bfac023788d9
Carlos Sales <causalvador@hotmail.com>
2017-01-22 21:29:19 -0300

    novo painel - em andamento

--- Pescadores/Pescadores/Painel.aspx
+++ Pescadores/Pescadores/Painel.aspx
@@ -5,6 +5,21 @@
     <br />
     <asp:Image ID="Image1" runat="server" ImageUrl="~/midias/pescador.JPG" />
     <br />
-    <h3 class="text-danger"><asp:Label ID="lblColonia" runat="server" Text=""></asp:Label></h3>
+    <h3 class="text-danger">
+        <asp:Label ID="lblColonia" runat="server" Text=""></asp:Label></h3>
+    <br />
+
+    <div class ="row">
+
+    <div class="panel panel-primary col-md-4">
+        <div class="panel-heading">
+            <h3 class="panel-title">Associados Cadastrados</h3>
+        </div>
+        <div class="panel-body">
+            <asp:Label ID="lblQuant" runat="server"></asp:Label>
+        </div>
+    </div>
+
+    </div>
 
 </asp:Content>
--- Pescadores/Pescadores/Painel.aspx.cs
+++ Pescadores/Pescadores/Painel.aspx.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Pescadores
 {
@@ -19,9 +15,23 @@ namespace Pescadores
                 // tenta identificar se houve login. caso contrário vai para página de erro
                 Colonia_Nome = Session["Colonia_Nome"].ToString();
                 lblColonia.Text = "Colônia: " + Colonia_Nome;
-
+                TotalRegistros();
             }
+        }
 
+        private void TotalRegistros()
+        {
+            OperacaoBanco operacao = new OperacaoBanco();
+            string stringselect = "select count(ID_Associado) as Quant " +
+                "from Tbl_Associados " +
+                "where ID_Colonia =" + Session["IDCol"].ToString();
+
+            System.Data.SqlClient.SqlDataReader dados = operacao.Select(stringselect);
+
+            while (dados.Read())
+            {
+               lblQuant.Text  = Convert.ToString(dados[0]);
+            }
         }
     }
 }
